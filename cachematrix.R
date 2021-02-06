d45c7d4527c67c01caf2914466e28facdf431d2e
Jenarboleda <jarboledat@unal.edu.co>
2021-02-05 21:50:43 -0500

    se suben primeros cambios

--- cachematrix.R
+++ cachematrix.R
@@ -4,6 +4,26 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+    ## initialize value of inverse matrix 
+    inversa <- NULL
+    
+    # set value of matrix from user input y
+    set <- function(y) {
+        # sets to global x value of matrix input y 
+        x <<- y
+        # clears inversematrix value if it exists
+        inversa <<- NULL
+    }
+    
+    # get value of matrix if it has been set
+    get <- function() {x}
+    # set value of the inverse of matrix
+    setinversa <- function(inversacalculada) {inversa <<-inversacalculada}
+    # get value of the inversematrix
+    getinversa <- function() {inversa}
+    list(set = set, get = get,
+         setinversa = setinversa,
+         getinversa = getinversa)
 
 }
 
@@ -11,5 +31,13 @@ makeCacheMatrix <- function(x = matrix()) {
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    #get value of inverse
+    inversa <- x$setinversa()
+    #check to see if inverse is already computed
+    if(!is.null(inversa)) {
+        #if inverse is cached, get the cached value and return
+        message("getting cached data")
+        return(reversa)
+    }
 }
